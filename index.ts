5ee686525e75a5cb7c05228a689a9e81de453383
unknown <793039998@qq.com>
2021-10-28 19:35:57 +0800

    faet: ts init 10-28

---
+++ index.ts
@@ -0,0 +1,14 @@
+/**
+ * @Author       : Humility
+ * @Date         : 2021-10-23 23:04:50
+ * @LastEditTime : 2021-10-28 19:16:09
+ * @LastEditors  : Humility
+ * @FilePath     : \humble-utils\index.ts
+ * @Description  : 通用工具类
+ */
+
+export * from "./src/array"
+export * from "./src/check"
+export * from "./src/date"
+export * from "./src/object"
+export * from "./src/string"
