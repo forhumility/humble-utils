73886896fa138d01a618663a4b1b152916c8c7ab
humility <793039998@qq.com>
2023-07-14 14:37:20 +0800

    perf: 优化循环语法 07-14

--- src/object.ts
+++ src/object.ts
@@ -1,12 +1,11 @@
 /**
  * @Author       : Humility
  * @Date         : 2021-10-23 23:36:50
- * @LastEditTime : 2021-10-30 14:59:16
+ * @LastEditTime : 2023-07-14 14:35:13
  * @LastEditors  : Humility
  * @FilePath     : \humble-utils\src\object.ts
  * @Description  : 对象
  */
-
 
 /**
  * 判断两个对象是否键值相同
@@ -16,19 +15,19 @@
  * @return {Boolean} 是否相同
  */
 export function isObjectEqual(obj1: object, obj2: object) {
-    let aProps = Object.getOwnPropertyNames(obj1);
-    let bProps = Object.getOwnPropertyNames(obj2);
+  let aProps = Object.getOwnPropertyNames(obj1);
+  let bProps = Object.getOwnPropertyNames(obj2);
 
-    if (aProps.length !== bProps.length) {
-        return false;
+  if (aProps.length !== bProps.length) {
+    return false;
+  }
+
+  for (const element of aProps) {
+    let propName = element;
+
+    if (obj1[propName] !== obj2[propName]) {
+      return false;
     }
-
-    for (let i = 0; i < aProps.length; i++) {
-        let propName = aProps[i];
-
-        if (obj1[propName] !== obj2[propName]) {
-            return false;
-        }
-    }
-    return true;
+  }
+  return true;
 }
